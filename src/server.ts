b03f53731532a2892c16c670ca97e59855c1b70a
aleksanderpalamar <palamarsolutionit@palamarsolutionit.com.br>
2022-09-20 11:18:59 -0300

    DEPLOY

---
+++ src/server.ts
@@ -0,0 +1,98 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
+import { convertHourStringToMinutes } from "./utils/convert-hour-string-to-minutes";
+import { convertMinutesToHourString } from "./utils/convert-minutes-to-hour-string";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const prisma = new PrismaClient();
+
+app.get("/games", async (req, res) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        }
+      }
+    },
+  });
+
+  return res.json(games);
+});
+
+app.post('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id;
+  const body: any = request.body;
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hoursStart: convertHourStringToMinutes(body.hourStart),
+      hoursEnd: convertHourStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    },
+  })
+
+  return response.status(201).json(ad);
+});
+
+app.get("/games/:id/ads", async (req, res) => {
+  const gameId = req.params.id; 
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hoursStart: true,
+      hoursEnd: true,      
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return res.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(","),
+      hoursStart: convertMinutesToHourString(ad.hoursStart),
+      hoursEnd: convertMinutesToHourString(ad.hoursEnd),
+    }
+  }));
+});
+
+app.get("/ads/:id/discord", async (req, res) => {
+  const adId = req.params.id;
+  
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    },
+  });
+
+  return res.json({
+    discord: ad.discord,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
